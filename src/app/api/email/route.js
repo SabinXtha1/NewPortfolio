f1604cf6665ebfa8d4c19febe392ea07fb943fc2
Sabin Nayaju <raj123nayaju@gmail.com>
2025-04-02 18:24:22 +0545

    jj

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -1,35 +1,41 @@
 import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer'
 
-export async function POST(req) {
-    try {
-        const { name, email, msg } = await req.json();
+export async function POST(req){
+    try{
 
-        if (!name || !email || !msg) {
-            return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
-        }
+        const {name,email,msg} = await req.json()
+        if(!name|| !email || !email){
+    return NextResponse.json({
+        error:'All Fields are required'
+    },{
+        status:400
+    })
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_FROM,
-                pass: process.env.PASS
-            }
-        });
-
-        const mailOptions = {
-            from: process.env.EMAIL_FROM, // Always use your verified email
-            to: process.env.EMAIL_FROM, // Your email
-            subject: 'Contact',
-            text: `Name: ${name}\nEmail: ${email}\nMessage: ${msg}`
-        };
-
-        await transporter.sendMail(mailOptions);
-
-        return NextResponse.json({ success: "Message sent successfully" }, { status: 200 });
-
-    } catch (error) {
-        console.error("Email sending error:", error);
-        return NextResponse.json({ error: "Error sending email" }, { status: 500 });
+}
+  const transpoter= nodemailer.createTransport({
+      service:'gmail',
+    auth:{
+        user:process.env.EMAIL_FROM,
+        pass:process.env.PASS
     }
+})
+
+const mailOptions={
+    from:email,
+    to:process.env.EMAIL_FROM,
+    subject:'Contact',
+    text:`
+    Name:${name},
+    Message:${msg}
+    `
+}
+await transpoter.sendMail(mailOptions)
+
+
+return NextResponse.json({ success: "Message sent successfully" }, { status: 200 })
+}catch{
+    return NextResponse.json({ error: "Error sending email" }, { status: 500 });
+}
+
 }
