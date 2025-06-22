706087211ec67f1d62ca68cd25d941dfe33440d4
SabinXtha1 <110241457+SabinXtha1@users.noreply.github.com>
2025-06-22 23:14:14 +0545

    Update Navbar.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
   const items = [
     { icon: <VscHome size={18}  className='text-red-300' />, label: 'Home', onClick: () => redirect('/') },
     { icon: <Terminal size={18} className='text-red-300'/>, label: 'Terminal', onClick: () => redirect('/Terminal') },
-    { icon: <FaRegAngry  size={18} className='text-red-300'/>, label: 'Profile', onClick: () => redirect('/profile') },
+    // { icon: <FaRegAngry  size={18} className='text-red-300'/>, label: 'Profile', onClick: () => redirect('/profile') },
     { icon: <Contact size={18} className='text-red-300'/>, label: 'Contact', onClick: () => redirect('/contact') },
   ];
   return (
